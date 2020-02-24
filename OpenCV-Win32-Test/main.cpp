f4c77e7da02c60222994559b85ae472f34c88e20
Denis Belobrotski <belobrotskiy@gmail.com>
2020-02-24 20:32:55 +0300

    All code refactored

--- OpenCV-Win32-Test/main.cpp
+++ OpenCV-Win32-Test/main.cpp
@@ -10,8 +10,6 @@
 #include <opencv2/videoio.hpp>
 
 
-void detectAndDisplay(cv::Mat frame);
-
 cv::CascadeClassifier face_cascade;
 cv::CascadeClassifier eyes_cascade;
 
@@ -62,6 +60,93 @@ std::string readTextFile(const std::string& filePath)
 }
 
 
+cv::Mat readImage(const std::string& filePath)
+{
+	cv::Mat image = cv::imread(filePath, cv::IMREAD_COLOR);
+
+	if (image.empty())
+	{
+		throw std::runtime_error("Can't read image.");
+	}
+
+	if (image.data == nullptr)
+	{
+		throw std::runtime_error("Bad image data.");
+	}
+
+	return image;
+}
+
+
+void ProcessFaceDetection(cv::Mat& sourceImage)
+{
+	cv::Mat grayscaledImage;
+	cv::cvtColor(sourceImage, grayscaledImage, cv::COLOR_BGR2GRAY);
+	cv::equalizeHist(grayscaledImage, grayscaledImage);
+
+	std::vector<cv::Rect> faces;
+	face_cascade.detectMultiScale(grayscaledImage, faces);
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		cv::Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+		cv::ellipse(sourceImage, center, cv::Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, cv::Scalar(255, 0, 255), 4);
+
+		cv::Mat faceSubimage = grayscaledImage(faces[i]);
+		std::vector<cv::Rect> eyes;
+		eyes_cascade.detectMultiScale(faceSubimage, eyes);
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			cv::Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+			int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25f);
+			cv::circle(sourceImage, eye_center, radius, cv::Scalar(255, 0, 0), 4);
+		}
+	}
+}
+
+
+void ProcessCameraImage()
+{
+	int cameraId = 0;
+	cv::VideoCapture capture(cameraId);
+	if (!capture.isOpened())
+	{
+		throw std::runtime_error("Can't use camera with id: " + std::to_string(cameraId));
+	}
+
+	cv::Mat frame;
+	while (capture.read(frame))
+	{
+		if (frame.empty())
+		{
+			throw std::runtime_error("Can't read frames from camera with id: " + std::to_string(cameraId));
+		}
+		
+		ProcessFaceDetection(frame);
+		cv::imshow("Runtime face detection", frame);
+
+		if (cv::waitKey(16.6) == 27)
+		{
+			break; // escape
+		}
+	}
+}
+
+
+void ProcessTestFaceImage()
+{
+	const std::string testImageFilePath = "face-small.jpg";
+
+	cv::Mat faceImage = readImage(testImageFilePath);
+
+	cv::imshow("Face image", faceImage);
+	ProcessFaceDetection(faceImage);
+	cv::imshow("Test face detection", faceImage);
+
+	cv::waitKey(0);
+}
+
+
 int main(int argc, const char** argv)
 {
 	try
@@ -84,29 +169,8 @@ int main(int argc, const char** argv)
 			throw std::runtime_error("Can't read eyes cascade");
 		}
 
-		//-- 2. Read the video stream
-		int cameraId = 0;
-		cv::VideoCapture capture(cameraId);
-		if (!capture.isOpened())
-		{
-			throw std::runtime_error("Can't use camera with id: " + std::to_string(cameraId));
-		}
-
-		cv::Mat frame;
-		while (capture.read(frame))
-		{
-			if (frame.empty())
-			{
-				std::cout << "--(!) No captured frame -- Break!\n";
-				break;
-			}
-			//-- 3. Apply the classifier to the frame
-			detectAndDisplay(frame);
-			if (cv::waitKey(10) == 27)
-			{
-				break; // escape
-			}
-		}
+		ProcessTestFaceImage();
+		ProcessCameraImage();
 	}
 	catch (const std::exception& e)
 	{
@@ -115,32 +179,4 @@ int main(int argc, const char** argv)
 	}
 	
 	return EXIT_SUCCESS;
-}
-
-
-void detectAndDisplay(cv::Mat frame)
-{
-	cv::Mat frame_gray;
-	cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-	//-- Detect faces
-	std::vector<cv::Rect> faces;
-	face_cascade.detectMultiScale(frame_gray, faces);
-	for (size_t i = 0; i < faces.size(); i++)
-	{
-		cv::Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, cv::Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, cv::Scalar(255, 0, 255), 4);
-		cv::Mat faceROI = frame_gray(faces[i]);
-		//-- In each face, detect eyes
-		std::vector<cv::Rect> eyes;
-		eyes_cascade.detectMultiScale(faceROI, eyes);
-		for (size_t j = 0; j < eyes.size(); j++)
-		{
-			cv::Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-			circle(frame, eye_center, radius, cv::Scalar(255, 0, 0), 4);
-		}
-	}
-	//-- Show what you got
-	imshow("Capture - Face detection", frame);
 }
